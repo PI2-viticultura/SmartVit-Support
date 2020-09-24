f2a5a8ef5b1715b8100f05126ee9911fba79c4cd
Adrianne Alves <driah_as@hotmail.com>
2020-09-24 08:23:07 -0300

    [Feature] Adding database communication files
    Co-authored-by: Lucas Vitor <lucasvitorifg@gmail.com>

---
+++ app/utils/constants.py
@@ -0,0 +1,9 @@
+DATABASE_CONFIG = {
+    "DB_HOST": "mongodbSupport",
+    "DB_PORT": 27017,
+    "DB_USERNAME": "admin",
+    "DB_PASSWORD": "password",
+    "authSource": "",
+    "authMechanism": "SCRAM-SHA-256",
+    "collection": "mensagens"
+}
